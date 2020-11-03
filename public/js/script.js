73c272cf6d192b11a9651b739c170cc4611ac54a
Agnes Chueng <agneschueng@gmail.com>
2020-11-03 11:21:27 -0600

    Added event listener on search button

--- public/js/script.js
+++ public/js/script.js
@@ -7,4 +7,16 @@ $( document ).ready(function() {
     // $(".sign-in-btn").hide();
     // $(".username").show();
     
+    $(".search-btn").on("submit", function(event) {
+        event.preventDefault();
+        let userSearch = $(".search-input").val().trim();
+        let zipSearch = $(".zip-code").val().trim();
+        $.ajax("/api/route", {
+            type: GET,
+            data: data
+        }).then(function() {
+
+        });
+    });
+
 });  
