37f6054e8c8dd26b76e059fa2ea4c3d59a48bd1f
Agnes Chueng <agneschueng@gmail.com>
2020-11-04 00:45:19 -0600

    changed get to post

--- controllers/controller.js
+++ controllers/controller.js
@@ -10,16 +10,17 @@ router.get("/", function(req,res){
 })
 
 //have to make async to wait for spotify to load results
-router.get("/songsearch", async function(req,res){
-
+router.post("/api/songsearch", async function(req,res){
     console.log('we hit the route!!', req.body);
     //"step up the morphine is test song to be replaced with user input"
-   let searchedSong = await spotifyHelper.spotifySongSearch("step up the morphine");
+   let searchedSong = await spotifyHelper.spotifySongSearch(req.body.song);
+   console.log("searchedSong:", searchedSong);
     //render entire obj
-   res.render("searchedSong", searchedSong)
+//    res.render("searchedSong", searchedSong)
+    res.json(searchedSong);
 
 })
-router.get("/api/albumsearch", async function(req,res){
+router.post("/api/albumsearch", async function(req,res){
 
     console.log('we hit the route!!', req.body);
     //"step up the morphine is test song to be replaced with user input"
@@ -28,7 +29,7 @@ router.get("/api/albumsearch", async function(req,res){
    res.render("searchedAlbum", searchedAlbum)
 
 })
-router.get("/api/artistsearch", async function(req,res){
+router.post("/api/artistsearch", async function(req,res){
 
     console.log('we hit the route!!', req.body);
     //"step up the morphine is test song to be replaced with user input"
