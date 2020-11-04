c9e3630392c1b5a3af963f9dd08e3bd32138ffc2
Agnes Chueng <agneschueng@gmail.com>
2020-11-04 00:58:48 -0600

    switch to determine what route to use for ajax call

--- public/js/script.js
+++ public/js/script.js
@@ -6,18 +6,48 @@ $( document ).ready(function() {
     // Else (user is logged in)
     // $(".sign-in-btn").hide();
     // $(".username").show();
-    
+
     //on click stuff!! do ajax call to send stuff from form to the backend!!! to that /search route!!
 
-    $(".search-btn").on("submit", function(event) {
+    $(".user-input").on("submit", function(event) {
         event.preventDefault();
-        let userSearch = $(".search-input").val().trim();
+        let musicSearch = $(".search-input").val().trim();
         let zipSearch = $(".zip-code").val().trim();
-        $.ajax("/api/route", {
-            type: GET,
-            data: data
-        }).then(function() {
+        let userSelect = $("#search-opt").val();
+        let searchFor;
+
+        switch (userSelect) {
+            case "Song": 
+                searchFor = "/api/songsearch"
+                break;
+            case "Artist":
+                searchFor = "/api/artistsearch"
+                break;
+            case "Album":
+                searchFor = "/api/albumsearch"
+                break;
+        }
+
+        // console.log(musicSearch);
+        $.ajax(searchFor, {
+            type: "POST",
+            data: {music: musicSearch}
+        }).then(function(res) {
             //.then(data)  of ajax call has all the spotifyResults from the backend route!! /search
+            console.log(res);
+            // search by song
+            // let song = $("<div>").text(res.Song);
+            // $("#song-info").html(song);
+
+            // search by artist
+            // let artist = $("<div>").text(res.name);
+            // $("#song-info").html(artist);
+
+            // search by album
+            // let album = $("<div>").text(res.albumName);
+            // $("#song-info").html(album);
+
+            // location.reload();
         });
     });
 
