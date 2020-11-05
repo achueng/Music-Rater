633e83443a117aaa7fa38d858a12265841bc4a5a
Agnes Chueng <68092570+achueng@users.noreply.github.com>
2020-11-05 11:50:38 -0600

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -29,16 +29,9 @@ router.post("/api/albumsearch", async function(req,res){
    let searchedAlbum = await spotifyHelper.spotifyAlbumSearch(req.body.music);
     //render entire obj
     res.json(searchedAlbum);
-
-<<<<<<< HEAD
-
-
-
-router.get("/search", function(req,res){
-=======
 })
+    
 router.post("/api/artistsearch", async function(req,res){
->>>>>>> 634b14297ffb457a7570a0af168a294dbe49409a
 
     console.log('we hit the route!!', req.body);
     //"step up the morphine is test song to be replaced with user input"
