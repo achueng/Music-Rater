169d5902598d0fe74d5d804bcab59568096dc2eb
Aaron Ray <aaray619@gmail.com>
2020-11-05 20:53:00 -0600

    propper connection

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,13 @@
 const mysql = require("mysql");
 
-var connection;
-if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {connection = mysql.createConnection({
-host: "localhost",
-port: 3306,
-user: "root",
-password: "password",
-database: "records_db"
-});}
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "records_db"
 });
+
+connection.connect();
 
 module.exports = connection;
