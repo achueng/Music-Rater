14db6b4c4f91f2808f3a9f2f5fdbe9e2cf1d0ef5
Aaron Ray <aaray619@gmail.com>
2020-11-05 20:52:17 -0600

    fixing merge issue

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,11 @@
-CREATE DATABASE records_db;
-
-use records_db;
+use jntpb1zjkgdskl33;
 
 CREATE TABLE topSongs(
     id int NOT NULL AUTO_INCREMENT,
     song varchar (50),
     artist varchar (50),
     album varchar (50),
+    createdat TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
 
@@ -14,11 +13,13 @@ CREATE TABLE topAlbums(
     id int NOT NULL AUTO_INCREMENT,
     album varchar (50),
     artist varchar (50),
+    createdat TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE topArtists(
     id int NOT NULL AUTO_INCREMENT,
     artist varchar (50),
+    createdat TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
